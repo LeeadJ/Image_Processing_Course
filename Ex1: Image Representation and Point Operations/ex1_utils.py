fb4ba1956c5e21f846883a7a9c01f4cbaf350350
Leead Jacobowitz <leead123@gmail.com>
2023-04-18 11:23:09 +0300

    imReadAndConvert/done

---
+++ Ex1: Image Representation and Point Operations/ex1_utils.py
@@ -0,0 +1,89 @@
+"""
+        '########:'##::::'##::::'##:::
+         ##.....::. ##::'##:::'####:::
+         ##::::::::. ##'##::::.. ##:::
+         ######:::::. ###::::::: ##:::
+         ##...:::::: ## ##:::::: ##:::
+         ##:::::::: ##:. ##::::: ##:::
+         ########: ##:::. ##::'######:
+        ........::..:::::..:::......::
+"""
+from typing import List
+import cv2 as cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+LOAD_GRAY_SCALE = 1
+LOAD_RGB = 2
+
+
+def myID() -> np.int:
+    """
+    Return my ID (not the friend's ID I copied from)
+    :return: int
+    """
+    return 313308785
+
+
+def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
+    """
+    Reads an image, and returns the image converted as requested
+    :param filename: The path to the image
+    :param representation: GRAY_SCALE or RGB
+    :return: The image object
+    """
+
+    image = cv2.imread(filename)
+    if representation == LOAD_GRAY_SCALE:  # (1)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    elif representation == LOAD_RGB:  # (2)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return (image - np.min(image)) / (np.max(image) - np.min(image))
+
+
+def imDisplay(filename: str, representation: int):
+    """
+    Reads an image as RGB or GRAY_SCALE and displays it
+    :param filename: The path to the image
+    :param representation: GRAY_SCALE or RGB
+    :return: None
+    """
+    pass
+
+
+def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
+    """
+    Converts an RGB image to YIQ color space
+    :param imgRGB: An Image in RGB
+    :return: A YIQ in image color space
+    """
+    pass
+
+
+def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
+    """
+    Converts an YIQ image to RGB color space
+    :param imgYIQ: An Image in YIQ
+    :return: A RGB in image color space
+    """
+    pass
+
+
+def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray):
+    """
+        Equalizes the histogram of an image
+        :param imgOrig: Original Histogram
+        :ret
+    """
+    pass
+
+
+def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
+    """
+        Quantized an image in to **nQuant** colors
+        :param imOrig: The original image (RGB or Gray scale)
+        :param nQuant: Number of colors to quantize the image to
+        :param nIter: Number of optimization loops
+        :return: (List[qImage_i],List[error_i])
+    """
+    pass

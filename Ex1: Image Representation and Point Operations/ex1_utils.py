755bba64dfdb85a3dc2ff5d918e8a1ea83b5a4fe
Leead Jacobowitz <leead123@gmail.com>
2023-04-18 12:34:08 +0300

    hsitogramEqualize/done

--- Ex1: Image Representation and Point Operations/ex1_utils.py
+++ Ex1: Image Representation and Point Operations/ex1_utils.py
@@ -81,7 +81,76 @@ def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarra
         :param imgOrig: Original Histogram
         :ret
     """
-    pass
+    # check if image is RGB.
+    image = np.copy(imgOrig) # making a copy of original the image
+    image_shape = image.shape # get the shape of the image
+    isRGB = False # init a flag to check if the image is RGB or not
+
+    # Check if the image has more than 2 dimensions. If so, it is RGB:
+    if len(image_shape) > 2:
+        # Convert image from RGB to YIQ
+        imgYIQ = transformRGB2YIQ(image)
+        # extracting the Y channel from imgYIQ (greyscale version of the image)
+        image = imgYIQ[:, :, 0]
+        isRGB = True
+
+    # Normalize the pixel values between 0 and 255
+    image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+    # Round the normalized pixel values to the nearest integer
+    image = (np.around(image)).astype('unit8')
+
+    # Calculating the image's histogram:
+    # Flatten the original image to a 1d array
+    image_flattened = image.ravel()
+    # Create an empty histogram array with 256 bins
+    histogram = np.zeros(256)
+    # Loop through each pixel value in the flattened image and update the histogram
+    for pixel_value in image_flattened:
+        histogram[pixel_value] += 1
+
+    # Calculate the cumulative sum of the histogram:
+    # Create an array of zeros with the same shape as the histogram
+    cumulative_sum = np.zeros_like(histogram)
+    # Set the first element of the cumulative sum array to be the same as the first element of the histogram
+    cumulative_sum[0] = histogram[0]
+    # Loop through the remaining elements of the histogram and calculate the cumulative sum
+    for i in range(1, len(histogram)):
+        cumulative_sum[i] = histogram[i] + cumulative_sum[i - 1]
+
+    # Create a lookup table for intensity transformation:
+    # Normalize cumulative sum
+    cumulative_sum_normalized = cumulative_sum / cumulative_sum.max()
+    # create a lookup table using normalized cumulative sum
+    look_up_table = (np.floor(cumulative_sum_normalized * 255)).astype('unit8')
+
+    # Apply the intensity transformation to the input image:
+    # Create an empty array for the equalized image
+    image_equalized = np.zeros_like(image, dtype=float)
+    # replace each intensity i with its corresponding intensity value in the lookup table
+    for i in range(256):
+        image_equalized[image == i] = look_up_table[i]
+
+    # Normalize and calculate the histogram of the equalized image
+    # normalize the equalized image
+    image_equalized = cv2.normalize(image_equalized, None, 0, 255, cv2.NORM_MINMAX)
+    # convert the equalized image to 8-bit integer type
+    image_equalized = (np.around(image_equalized)).astype('unit8')
+    # flatten the equalized image
+    image_equalized_flat = image_equalized.ravel()
+    # create an empty array for the histogram of the equalized image
+    histogram_equalized = np.zeros(256)
+    # calculate the histogram of the equalized image
+    for i in image_equalized_flat:
+        histogram_equalized[i] += 1
+
+    # If the original image was RGB, convert it back to RGB after equalization
+    if isRGB:
+        # Convert Y channel back to [0,1] range and replace it in the YIQ array
+        imgYIQ[:, :, 0] = image_equalized / 255
+        # Convert the YIQ image back to RGB and return it
+        image_equalized = transformYIQ2RGB(imgYIQ)
+
+    return image_equalized, histogram, histogram_equalized
 
 
 def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
